eee1b0e170a9eff8986c3b1a19f4b8a5c2d3b26c
Leandro Pessini <leandro@pessini.me>
2015-02-12 18:58:15 -0200

    First commit

--- ListViewController.h
+++ ListViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ListViewController.h
 //  DreamCatcher
 //
 //  Created by Leandro Pessini on 2/12/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ListViewController : UIViewController
 
 @end
-
